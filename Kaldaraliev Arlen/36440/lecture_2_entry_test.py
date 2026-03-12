403dc93716a2d49a4da0c54558f499ea0c576d95
miles3lsp <146581650+miles3lsp@users.noreply.github.com>
2026-03-12 10:04:32 +0000

    lecture2 entry test

---
+++ Kaldaraliev Arlen/36440/lecture_2_entry_test.py
@@ -0,0 +1,16 @@
+
+my_birthday = 9
+
+print(my_birthday)
+print(type(my_birthday))
+
+my_name = "Kaldaraliev Arlen"
+
+print(my_name)
+print(type(my_name))
+
+grade = 3.5
+
+grade_int = int(grade)
+print(grade_int) 
+print(type(grade_int))
